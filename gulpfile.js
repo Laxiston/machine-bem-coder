c3a782b146229580b9bd4c6f3862b1a402460e3c
alexbaum <alexbaum@yandex-team.ru>
2015-09-16 12:10:03 +0300

    scaffold for gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+
+/* TODO: */
+
+gulp.task('default', function() {
+    // place code for your default task here
+});
+
+gulp.task('server', function() {
+    // run develop server
+});
+
+gulp.task('compile', function() {
+    // enb make all
+});
+
+gulp.task('build', function() {
+    // compile + move all needed files to public_html directory
+});
+
+gulp.task('img', function() {
+    // optimize all css images
+});
+
+gulp.task('clean', function() {
+    // clean previous builds
+});
+
+gulp.task('watch', function() {
+    // watch project level blocks and compile
+});
